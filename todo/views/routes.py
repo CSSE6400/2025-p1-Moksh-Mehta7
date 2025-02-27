d056aa4dcc8160d8b6770675c49cd1a0044c8c38
Moksh-Mehta7 <moksh.mehta@uq.net.au>
2025-02-27 10:12:05 +1000

    Week1 3

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -3,9 +3,10 @@ from flask import Blueprint, jsonify
  
 api = Blueprint('api', __name__, url_prefix='/api/v1') 
  
-@api.route('/health') 
-def health(): 
-    return "ok"
+@api.route('/health')
+def health():
+ return jsonify({"status": "ok"})
+
 @api.route('/todos', methods=['GET']) 
 def get_todos(): 
     return jsonify([{ 
